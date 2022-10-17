15ef28cac3409e55df32c212f410e7063127f5b4
Malkahiro <tahiro9011@gmail.com>
2022-10-17 10:28:32 -0500

    Initial expense item created and styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import ExpenseItem from "./Components/ExpenseItem";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ExpenseItem />
     </div>
   );
 }
